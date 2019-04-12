70f012397639bb1516652d21194b42aec4108072
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-04-12 14:48:07 +0200

    Inclusion du formulaire d'expiration de contrat de location

--- accueil.php
+++ accueil.php
@@ -32,7 +32,6 @@
                                                 <li><a href='#contrat_de_location'>Contrat de location</a></li>
                                                 <li><a href='#attestation'>Attestation</a></li>
                                                 <li><a href='#relance_impayee'>Relance impayée</a></li>
-                                                <li><a href='#expiration_de_contrat_de_location'>Expiration de contrat de location</a></li>
                                                 <li><a href='#accuses_de_reception'>Accusés de réception</a></li>
                                             </ul>
                                             <div id='bienvenue'>
@@ -47,7 +46,6 @@
                                                         <li>Voyez les locations à venir</li>
                                                         <li>Générez des contrats de location</li>
                                                         <li>Générez des attestations</li>
-                                                        <li>Gérez les expirations des contrats de location</li>
                                                         <li>Générez des relances d'impayées</li>
                                                         <li>Générez des lettres d'expiration de contrat de location</li>
                                                         <li>Générez des accusés de réception quand des gens envoient leurs préavis de départ</li>
@@ -155,6 +153,66 @@
                                                                     <p>Si vous êtes d'accord, <input type='submit' class='text-warning ui-button ui-corner-all ui-widget' name='soumission_du_formulaire_de_generation_de_PDF' value='cliquez ici'></p>
                                                                     <p>Bonne journée,</p>
                                                                     <p>Suiviloc</p>
+                                                                </form>
+                                                            </p>
+                                                        </div>
+                                                        <h3>Expiration de contrat de location</h3>
+                                                        <div>
+                                                            <p>
+                                                                <form action='generation_d_un_document_PDF.php' method='post'>
+                                                                    <input type='hidden' name='type_de_document' value='expiration_de_contrat_de_location'>
+                                                                    <p>Bonjour,</p>
+                                                                    <p>Le contrat de location de <input type='text' name='nom_de_famille_du_locataire' class='ui-corner-all text-warning'> <input type='text' name='prenom_du_locataire' class='ui-corner-all text-warning'>, occupant le studio n°<select name='numero_du_studio_du_locataire' class='text-warning ui-corner-all' id='numero_du_studio_du_locataire' required>";
+
+            //
+            for ($incrementeur_des_surfaces = 1; $incrementeur_des_surfaces < 3; $incrementeur_des_surfaces++) {
+
+                //
+                if ($incrementeur_des_surfaces == 1) {
+
+                    $corps_de_la_page_html .= "<optgroup label='15m2'>";
+
+                } else {
+
+                    $corps_de_la_page_html .= "<optgroup label='18m2'>";
+
+                }
+
+                //
+                //$requete_preparee_pour_afficher_pour_selection_les_studios_disponibles = connexion_a_la_base_de_donnees_via_PDO::getinstance()->prepare("SELECT Studio.numero_du_studio FROM Studio INNER JOIN Contrat ON Contrat.studio WHERE Contrat.studio != Studio.id AND Studio.surface = :id_de_la_surface");
+
+                //
+                $requete_preparee_pour_afficher_pour_selection_les_studios_disponibles = connexion_a_la_base_de_donnees_via_PDO::getinstance()->prepare("SELECT Studio.numero_du_studio FROM Studio WHERE Studio.surface = :id_de_la_surface");
+
+                //
+                $requete_preparee_pour_afficher_pour_selection_les_studios_disponibles->bindParam(":id_de_la_surface", $incrementeur_des_surfaces);
+
+                //
+                $requete_preparee_pour_afficher_pour_selection_les_studios_disponibles->execute();
+
+                //
+                $resultat_de_la_requete = $requete_preparee_pour_afficher_pour_selection_les_studios_disponibles->fetchAll(PDO::FETCH_BOTH);
+
+                //
+                $nombre_de_resultats_dans_la_requete = $requete_preparee_pour_afficher_pour_selection_les_studios_disponibles->rowCount();
+
+                //
+                for ($i = 0; $i < $nombre_de_resultats_dans_la_requete; $i++) {
+
+                    //
+                    $corps_de_la_page_html .= "<option value='" . $resultat_de_la_requete[$i][0] . "'>" . $resultat_de_la_requete[$i][0] . "</option>";
+
+                }
+
+                //
+                $corps_de_la_page_html .= "</optgroup>";
+
+            }
+
+            $corps_de_la_page_html .= "</select>, se termine le <input type='text' name='date_de_fin_du_contrat_de_location' class='text-warning calendrier_pour_faire_un_choix_de_date ui-corner-all' required>.</p>
+                                                                    <p>Si vous êtes d'accord, <input type='submit' class='text-warning ui-button ui-corner-all ui-widget' name='soumission_du_formulaire_de_generation_de_PDF' value='cliquez ici'></p>
+                                                                    <p>Bonne journée,</p>
+                                                                    <p>Suiviloc</p>                                                            
                                                                 </form>
                                                             </p>
                                                         </div>
@@ -322,9 +380,6 @@
                                                             <p>45545</p>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div id='expiration_de_contrat_de_location'>
-                                                    
                                                 </div>
                                                 <div id='accuses_de_reception'>
                                                     <div class='menu_en_accordeon'>
