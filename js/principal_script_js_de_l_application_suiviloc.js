2ee120296276700f799bea8b365bc05ddbc20407
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-04-26 13:41:01 +0200

    Traduction du framework DataTable en français

--- js/principal_script_js_de_l_application_suiviloc.js
+++ js/principal_script_js_de_l_application_suiviloc.js
@@ -340,6 +340,26 @@ $(document).ready(function(){
         "searching": false,
         "scrollY":        200,
         "scrollCollapse": true,
-        "jQueryUI":       true
+        "jQueryUI":       true,
+        "language": {
+            "sProcessing": "Traitement en cours ...",
+            "sLengthMenu": "Afficher _MENU_ lignes",
+            "sZeroRecords": "Aucun résultat trouvé",
+            "sEmptyTable": "Aucune donnée disponible",
+            "sInfo": "Lignes _START_ à _END_ sur _TOTAL_",
+            "sInfoEmpty": "Aucune ligne affichée",
+            "sInfoFiltered": "(Filtrer un maximum de_MAX_)",
+            "sInfoPostFix": "",
+            "sSearch": "Chercher:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Chargement...",
+            "oPaginate": {
+                "sFirst": "Premier", "sLast": "Dernier", "sNext": "Suivant", "sPrevious": "Précédent"
+            },
+            "oAria": {
+                "sSortAscending": ": Trier par ordre croissant", "sSortDescending": ": Trier par ordre décroissant"
+            }
+        }
     });
 });
