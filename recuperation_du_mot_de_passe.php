53acaa3805cf3fdcdb43ab4cc2e7822dc0a4cbbb
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-04-28 16:12:27 +0200

    Harmonisation de tous les en-têtes des pages HTML

--- recuperation_du_mot_de_passe.php
+++ recuperation_du_mot_de_passe.php
@@ -118,6 +118,8 @@
                                                 <link rel='stylesheet' href='css/jquery-ui.structure.min.css'>
                                                 <link rel='stylesheet' href='css/jquery-ui.min.css'>
                                                 <link href='css/feuille_de_style_principale_de_l_application_suiviloc.css' rel='stylesheet' type='text/css'>
+                                                <link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css\">
+                                                <script type=\"text/javascript\" charset=\"utf8\" src=\"https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js\"></script>
                                                 <script>
                                                     $( function() {
                                                         $('#information_de_refus_de_changement_de_mot_de_passe').dialog({
